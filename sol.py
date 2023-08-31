92e8e6bf7a5d68d75f306c2ae2b50b4c9bbdc90c
Andi <andreas.rajchert@dci-student.org>
2023-08-31 13:54:28 +0200

    finish

---
+++ sol.py
@@ -0,0 +1,16 @@
+
+text = "hello world"
+text2 = "hallo planet"
+
+
+if len(text) % 2:
+    print(f"odd")
+else:
+    print(f"even")
+
+
+if len(text2) % 2:
+    print(f"odd")
+else:
+    print(f"even")
+
